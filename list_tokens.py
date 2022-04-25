89bd28afbae9ca7704f159a48aa8a00534d1b86d
AstraL13666 <69980366+AstraL13666@users.noreply.github.com>
2022-04-25 15:58:11 +0300

    Update list_tokens.py

--- list_tokens.py
+++ list_tokens.py
@@ -1,2 +1,4 @@
-token_wm = '9bd50d2406fa3d70ac143dfda9d82cb'
-token_tg = '337968158:AAFrrm5f_ee6bPZhm97iJYgp9zDqhFs9-lI'
+# токены битые =)
+
+token_wm = '9bd50d2406fa3d70ac143dfda9d82cb'  # Токен сайта
+token_tg = '337968158:AAFrrm5f_ee6bPZhm97iJYgp9zDqhFs9-lI'  # Токен телеграм
